a156830c770f02d6456aefef981d3d79379fd0cb
DrInnovatio <turing1723@gmail.com>
2021-10-04 23:01:38 +0900

    how to set up variabes.

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,12 @@
  let js = 'amazing';
       if (js === 'amazing')
         alert('JS is fun!!')
+
+let firstName = "John"
+let lastName = "Martin"
+
+console.log(firstName + " " + lastName)
+
+let person = "Jane"
+let PI = 3.1415;
+
