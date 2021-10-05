e736dff3d59972b5d9c6be6d32255c9e13c27632
DrInnovatio <turing1723@gmail.com>
2021-10-05 22:09:10 +0900

    practiced an object.

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -10,3 +10,12 @@ console.log(firstName + " " + lastName)
 let person = "Jane"
 let PI = 3.1415;
 
+// Data Types.
+
+let me = {
+  name: "JBP",
+  age: 40,
+  education: "master"
+}
+
+console.log(me)
