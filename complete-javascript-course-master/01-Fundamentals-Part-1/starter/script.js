2978b3217acd642f76c15889a143dbb29fba2633
DrInnovatio <turing1723@gmail.com>
2021-10-31 20:15:32 +0900

    using if statement to compare the teams' scores.

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -230,3 +230,11 @@ console.log(hasDriversLicense || hasGoodVision || isTired);
 const scoreDolphins = (96 + 108 + 89) / 3;
 const scoreKoalas = (88 + 91 + 110) / 3;
 console.log(scoreDolphins, scoreKoalas)
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolohins win the trophy!")
+} else if (scoreKoalas > scoreDolphins) {
+  console.log("Koalas win the trophy!")
+} else if (scoreDolphins === scoreKoalas) {
+  console.log("Both the win the trophy!!")
+}
